b60d51fdd566c59e0a2570d88cac3c8a9491f835
takuya matsunaga <takanotume24@gmail.com>
2019-11-09 20:59:48 +0900

    RTCの時刻を送信できるように変更

--- src/main.cpp
+++ src/main.cpp
@@ -11,41 +11,33 @@
 #define S_PERIOD 1                 // Silent period
 RTC_DATA_ATTR static uint8_t seq;  // remember number of boots in RTC Memory
 
+struct tm now;
 time_t tm_t;
-void setAdvData(BLEAdvertising *, struct tm *);
+void setAdvData(BLEAdvertising *pAdvertising, struct tm now);
 
-void task_ble(void *pvParameters) {
-  struct tm *now = (struct tm *)pvParameters;
+void task_ble() {
   BLEDevice::init("AmbientEnv-02");                // デバイスを初期化
   BLEServer *pServer = BLEDevice::createServer();  // サーバーを生成
   BLEAdvertising *pAdvertising =
       pServer->getAdvertising();  // アドバタイズオブジェクトを取得
   setAdvData(pAdvertising, now);
-
   pAdvertising->start();   // アドバタイジングデーターをセット
   delay(T_PERIOD * 1000);  // T_PERIOD秒アドバタイズする
   pAdvertising->stop();
-  seq++;  // シーケンス番号を更新
-
-  M5.Axp.ScreenBreath(0);
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_37, LOW);
-  esp_deep_sleep_start();
 }
 
-void task_lcd(void *pvParameters) {
-  struct tm *now = (struct tm *)pvParameters;
-
-  M5.Axp.ScreenBreath(10);   // 画面の輝度を下げる
-  M5.Lcd.setRotation(3);     // 左を上にする
-  M5.Lcd.setTextSize(2);     // 文字サイズを2にする
-  M5.Lcd.fillScreen(BLACK);  // 背景を黒にする
-  M5.Lcd.setCursor(0, 0, 1);
+void task_lcd() {
+  M5.begin();
+  M5.Axp.ScreenBreath(10);  // 画面の輝度を下げる
+  M5.Lcd.setRotation(3);    // 左を上にする
+  M5.Lcd.setTextSize(3);  // 文字サイズを2にする
   M5.Lcd.printf("seq: %d\r\n", seq);
-  M5.Lcd.printf("tm: %d:%d:%d'\r\n", now->tm_hour, now->tm_min, now->tm_sec);
+  M5.Lcd.printf("tm:\r\n"); 
+  M5.Lcd.printf("%d:%d:%d'\r\n", now.tm_hour, now.tm_min, now.tm_sec);
 }
 
-void setAdvData(BLEAdvertising *pAdvertising, struct tm *now) {
-  tm_t = mktime(now);
+void setAdvData(BLEAdvertising *pAdvertising, struct tm now) {
+  tm_t = mktime(&now);
 
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
 
@@ -57,7 +49,7 @@ void setAdvData(BLEAdvertising *pAdvertising, struct tm *now) {
   strServiceData += (char)0xff;  // AD Type 0xFF: Manufacturer specific data
   strServiceData += (char)0xff;  // Test manufacture ID low byte
   strServiceData += (char)0xff;  // Test manufacture ID high byte
-  strServiceData += (char)seq;
+  strServiceData += (char)seq++;
   strServiceData += (char)(tm_t & 0xff);
   strServiceData += (char)((tm_t >> 8) & 0xff);
   strServiceData += (char)((tm_t >> 16) & 0xff);
@@ -68,14 +60,13 @@ void setAdvData(BLEAdvertising *pAdvertising, struct tm *now) {
 }
 
 void setup() {
-  M5.begin();
-  Wire.begin();
-  struct tm now;
-
   pinMode(GPIO_NUM_37, INPUT_PULLUP);
-  getLocalTime(&now);
-  xTaskCreatePinnedToCore(task_ble, "task_ble", 10000, &now, 1, NULL, 0);
-  xTaskCreatePinnedToCore(task_lcd, "task_lcd", 10000, &now, 2, NULL, 0);
+  getLocalTime(&now,100);
+  task_lcd();
+  task_ble();
+  M5.Axp.ScreenBreath(0);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_37, LOW);
+  esp_deep_sleep_start();
 }
 
 void loop() {}
