37e6298de609063b4e6e0015c1f9b4ebf70c45d3
takuya matsunaga <takanotume24@gmail.com>
2019-11-09 19:35:03 +0900

    画面のチラツキを解決するためにマルチタスクにしたが動かなかった｡

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,81 @@
 #include <M5StickC.h>
 #include <Wire.h>
-
 #include "BLEDevice.h"
 #include "BLEServer.h"
 #include "BLEUtils.h"
 #include "esp_sleep.h"
+#include "sys/time.h"
 
+#define T_PERIOD 5
+// Transmission period
+#define S_PERIOD 1                 // Silent period
+RTC_DATA_ATTR static uint8_t seq;  // remember number of boots in RTC Memory
 
-#define T_PERIOD     5
-   // Transmission period
-#define S_PERIOD     1  // Silent period
-RTC_DATA_ATTR static uint8_t seq; // remember number of boots in RTC Memory
+time_t tm_t;
+void setAdvData(BLEAdvertising *, struct tm *);
 
-uint16_t temp;
-uint16_t humid;
-uint16_t press;
-uint16_t vbat;
+void task_ble(void *pvParameters) {
+  struct tm *now = (struct tm *)pvParameters;
+  BLEDevice::init("AmbientEnv-02");                // デバイスを初期化
+  BLEServer *pServer = BLEDevice::createServer();  // サーバーを生成
+  BLEAdvertising *pAdvertising =
+      pServer->getAdvertising();  // アドバタイズオブジェクトを取得
+  setAdvData(pAdvertising, now);
 
-void setAdvData(BLEAdvertising *pAdvertising) {
-    BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  pAdvertising->start();   // アドバタイジングデーターをセット
+  delay(T_PERIOD * 1000);  // T_PERIOD秒アドバタイズする
+  pAdvertising->stop();
+  seq++;  // シーケンス番号を更新
 
-    oAdvertisementData.setFlags(0x06); // BR_EDR_NOT_SUPPORTED | LE General Discoverable Mode
+  M5.Axp.ScreenBreath(0);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_37, LOW);
+  esp_deep_sleep_start();
+}
 
-    std::string strServiceData = "";
-    strServiceData += (char)0x0c;   // 長さ
-    strServiceData += (char)0xff;   // AD Type 0xFF: Manufacturer specific data
-    strServiceData += (char)0xff;   // Test manufacture ID low byte
-    strServiceData += (char)0xff;   // Test manufacture ID high byte
-    strServiceData += (char)seq;                   // シーケンス番号
-    strServiceData += (char)(temp & 0xff);         // 温度の下位バイト
-    strServiceData += (char)((temp >> 8) & 0xff);  // 温度の上位バイト
-    strServiceData += (char)(humid & 0xff);        // 湿度の下位バイト
-    strServiceData += (char)((humid >> 8) & 0xff); // 湿度の上位バイト
-    strServiceData += (char)(press & 0xff);        // 気圧の下位バイト
-    strServiceData += (char)((press >> 8) & 0xff); // 気圧の上位バイト
-    strServiceData += (char)(vbat & 0xff);         // 電池電圧の下位バイト
-    strServiceData += (char)((vbat >> 8) & 0xff);  // 電池電圧の上位バイト
+void task_lcd(void *pvParameters) {
+  struct tm *now = (struct tm *)pvParameters;
 
-    oAdvertisementData.addData(strServiceData);
-    pAdvertising->setAdvertisementData(oAdvertisementData);
+  M5.Axp.ScreenBreath(10);   // 画面の輝度を下げる
+  M5.Lcd.setRotation(3);     // 左を上にする
+  M5.Lcd.setTextSize(2);     // 文字サイズを2にする
+  M5.Lcd.fillScreen(BLACK);  // 背景を黒にする
+  M5.Lcd.setCursor(0, 0, 1);
+  M5.Lcd.printf("seq: %d\r\n", seq);
+  M5.Lcd.printf("tm: %d:%d:%d'\r\n", now->tm_hour, now->tm_min, now->tm_sec);
+}
+
+void setAdvData(BLEAdvertising *pAdvertising, struct tm *now) {
+  tm_t = mktime(now);
+
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+
+  oAdvertisementData.setFlags(
+      0x06);  // BR_EDR_NOT_SUPPORTED | LE General Discoverable Mode
+
+  std::string strServiceData = "";
+  strServiceData += (char)8;     // 長さ
+  strServiceData += (char)0xff;  // AD Type 0xFF: Manufacturer specific data
+  strServiceData += (char)0xff;  // Test manufacture ID low byte
+  strServiceData += (char)0xff;  // Test manufacture ID high byte
+  strServiceData += (char)seq;
+  strServiceData += (char)(tm_t & 0xff);
+  strServiceData += (char)((tm_t >> 8) & 0xff);
+  strServiceData += (char)((tm_t >> 16) & 0xff);
+  strServiceData += (char)((tm_t >> 24) & 0xff);
+
+  oAdvertisementData.addData(strServiceData);
+  pAdvertising->setAdvertisementData(oAdvertisementData);
 }
 
 void setup() {
-    M5.begin();
-    M5.Axp.ScreenBreath(10);      // 画面の輝度を下げる
-    M5.Lcd.setRotation(3);      // 左を上にする
-    M5.Lcd.setTextSize(2);      // 文字サイズを2にする
-    M5.Lcd.fillScreen(BLACK);   // 背景を黒にする
-    pinMode(GPIO_NUM_37,INPUT_PULLUP);
-    Wire.begin();               // I2Cを初期化する
+  M5.begin();
+  Wire.begin();
+  struct tm now;
 
-
-    temp++;
-    humid++;
-    press++;
-    vbat = (uint16_t)(M5.Axp.GetVbatData() * 1.1 / 1000 * 100);
-
-    M5.Lcd.setCursor(0, 0, 1);
-    M5.Lcd.printf("temp: %4.1f'C\r\n", (float)temp / 100);
-    M5.Lcd.printf("humid:%4.1f%%\r\n", (float)humid / 100);
-    M5.Lcd.printf("press:%4.0fhPa\r\n", (float)press / 10);
-    M5.Lcd.printf("vbat: %4.2fV\r\n", (float)vbat / 100);
-
-    BLEDevice::init("AmbientEnv-02");                  // デバイスを初期化
-    BLEServer *pServer = BLEDevice::createServer();    // サーバーを生成
-
-    BLEAdvertising *pAdvertising = pServer->getAdvertising(); // アドバタイズオブジェクトを取得
-    setAdvData(pAdvertising);                          // アドバタイジングデーターをセット
-
-    pAdvertising->start();                             // アドバタイズ起動
-    delay(T_PERIOD * 1000);                            // T_PERIOD秒アドバタイズする
-    pAdvertising->stop();                              // アドバタイズ停止
-
-    seq++;                                             // シーケンス番号を更新
-    delay(10);
-    esp_sleep_enable_ext0_wakeup(GPIO_NUM_37,LOW);
-    M5.Axp.ScreenBreath(0);
-    // M5.Axp.DeepSleep();              // S_PERIOD秒Deep Sleepする
-    // esp_deep_sleep(1000000LL * S_PERIOD);              // S_PERIOD秒Deep Sleepする
-    esp_deep_sleep_start();
+  pinMode(GPIO_NUM_37, INPUT_PULLUP);
+  getLocalTime(&now);
+  xTaskCreatePinnedToCore(task_ble, "task_ble", 10000, &now, 1, NULL, 0);
+  xTaskCreatePinnedToCore(task_lcd, "task_lcd", 10000, &now, 2, NULL, 0);
 }
 
-void loop() {
-}
+void loop() {}
